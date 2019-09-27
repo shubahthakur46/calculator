0b635437d2625b34a0472c82d7d6b1659456e37f
shubahthakur46 <shubahthakur46@gmail.com>
2019-09-27 11:55:15 +0600

    first commit

---
+++ app/src/main/java/com/example/shubah/calculator/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.shubah.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.txt1);
+    }
+
+    public void hello(View v){
+        txt.setText("shubah");
+    }
+
+}
